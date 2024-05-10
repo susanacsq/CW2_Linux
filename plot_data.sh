c3515bef5efca02ea8f16c8a4f9af07e05e711cd
susanacsq <129346698+susanacsq@users.noreply.github.com>
2024-05-10 14:54:05 +0100

    Update plot_data.sh

--- plot_data.sh
+++ plot_data.sh
@@ -1,3 +1,4 @@
+  GNU nano 5.4                                                                                 plot_data.sh                                                                                          
 #!/bin/bash
 
 MYSQL_HOST="localhost"
@@ -6,21 +7,24 @@ MYSQL_PASSWORD="1234"
 MYSQL_DATABASE="bitcointracker"
 
 plot_bitcoin_price_changes() {
-    mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE>
-SELECT timestamp, current_price FROM bitcoin_prices ORDER BY timestamp;
-EOF
+    mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e "SELECT timestamp, current_price FROM bitcoin_prices ORDER BY timestamp;" | tail -n +2 > bitcoin_prices.dat
+
     gnuplot -p -e "
-    set title 'Bitcoin Price Changes';
-    set xlabel 'Date';
-    set ylabel 'Rates (USD)';
-    set xdata time;
-    set timefmt '%Y-%m-%d';
-    set format x '%Y-%m-%d';
-    plot 'bitcoin_prices.dat' using 1:3 with lines;
-"
+        set title 'Bitcoin Price Changes';
+        set xlabel 'Date';
+        set ylabel 'Rates (USD)';
+        set xdata time;
+        set timefmt '%Y-%m-%d';
+        set format x '%Y-%m-%d';
+        set output 'bitcoin_price_changes.png';
+        set xtics 3*24*3600;
+        plot 'bitcoin_prices.dat' using 1:3 with lines;
+    "
 }
 
 plot_bitcoin_price_changes
+
+
 
 
 
